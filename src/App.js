0e83f5c48815e3af5d4ff56ac3b53bb0e4ec88dd
ge <uc5804@naver.com>
2022-03-31 01:15:36 +0900

    라우팅

--- src/App.js
+++ src/App.js
@@ -1,67 +1,44 @@
-/*eslint-disable*/
+/* eslint-disable */
 import React, {useState} from 'react';
-import './App.css';
 import {Navbar, Container, Nav, NavDropdown, Offcanvas, Button, Form, FormControl} from 'react-bootstrap';
+import './App.css';
+import Data from './data.js';
 import {Link, Route, Switch} from 'react-router-dom';
- 
+import Detail from './Detail.js';
+
 function App() {
   let [shoes, shoes변경] = useState(Data);
   return (
-    <>
-   <Navbar bg="light" expand={false}>
-    <Container fluid>
-      <Navbar.Brand href="#">ShoeShop</Navbar.Brand>
-      <Navbar.Toggle aria-controls="offcanvasNavbar" />
-      <Navbar.Offcanvas
-        id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel"
-        placement="end"
-      >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Nav className="justify-content-end flex-grow-1 pe-3">
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form className="d-flex">
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Offcanvas.Body>
-      </Navbar.Offcanvas>
-    </Container>
-  </Navbar>
+    <div>
+   <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">ShoShop</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link><Link to="/">Home</Link></Nav.Link>
+        <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
 
-   {/* 점보 */}
+
+
+  <Route exact path="/">
+    {/* 점보 */}
   <div className="background">
     <h1 style={{textAlign : "center"}}>20% Season Off</h1>
     <p style = {{textAlign : "center"}}>This is simple hero unit, single information </p>
     <Button variant="primary" style={{display : 'block', marginLeft : 'auto', marginRight  :'auto'}}>More</Button>{' '}
   </div>
-
-  <Route path="/">
-    <div>메인페이지</div>
-  </Route> 
-  <Route path="/detail">
-    <div>디테일페이지</div>
-  </Route>
-
-
 
   <div className="container">
     <div className="row">
@@ -72,16 +49,25 @@ function App() {
     }
     </div>
   </div>
-  </>
+  </Route>
+
+  <Route path="/detail" >
+    <Detail />
+  </Route>
+
+  {/* <Route path="어쩌구" component={Modal}></Route> */}
+
+  </div>
   );
 }
+
 
 function Card(props){
   return (
     <div className="col-md-4">
       <img src={'https://codingapple1.github.io/shop/shoes'+ (props.i +1 ) +'.jpg'} width="100%"/>
-      <h4>{props.shoes.title}</h4>
-      <p>{props.shoes.content} & {props.shoes.price}</p>
+      <h4 style={{textAlign : "center"}}>{props.shoes.title}</h4>
+      <p style={{textAlign : "center"}}>{props.shoes.content} & {props.shoes.price}</p>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorker';
 
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -19,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
